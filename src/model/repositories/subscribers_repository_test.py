aa2ed77d12933d9c2a55118569ebd64c18dfc8d1
ronaldandrade <andrade.ronald@gmail.com>
2025-02-19 21:25:57 -0300

    adding new methods in subs_repo

--- src/model/repositories/subscribers_repository_test.py
+++ src/model/repositories/subscribers_repository_test.py
@@ -4,8 +4,8 @@ from .subscribers_repository import SubscribersRepository
 @pytest.mark.skip('Insert in DB')
 def test_insert_subscriber():
     subscriber_infos = {
-        'nome': 'João',
-        'email': 'email@email.com',
+        'nome': 'Pao',
+        'email': 'pao@email.com',
         'evento_id': 1
     }
 
@@ -20,3 +20,12 @@ def test_select_subscriber():
         subs_repo = SubscribersRepository()
         resp = subs_repo.select_subscriber(email, evento_id)
         print(resp.nome)
+
+@pytest.mark.skip('ranking link_subscriber in DB')
+def test_ranking():
+      evento_id = 1
+      subs_repo = SubscribersRepository()
+      resp  = subs_repo.get_ranking(evento_id)
+
+      for elem in resp:
+            print(f'Link: {elem.link},  total de inscritos: {elem.total}')
